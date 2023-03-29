eeb5ac16ed9804f9058332a44a0c1550c8090188
gokulkrishnav <gokulkrishnav342000@gmail.com>
2023-03-30 00:28:25 +0530

    DSA Problems

--- src/Arrays/Convert_Integer_Tor_Binary.java
+++ src/Arrays/Convert_Integer_Tor_Binary.java
@@ -8,11 +8,14 @@ public class Convert_Integer_Tor_Binary {
 
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] arr = new int[7];
-     int temp = n % 2;
+    int[] arr = new int[8];
+     for(int i = 0; n > 0; i++){
+         arr[7 - i] = n % 2;
+         n = n / 2;
+     }
 
-
-
-
+     for(int j = 0;j < arr.length;j++){
+         System.out.print(arr[j] + " ");
+     }
 }
 }
