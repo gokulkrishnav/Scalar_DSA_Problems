bd09d68974ea3f53a8dcc7123c4c518683e8bf6b
gokulkrishnav <gokulkrishnav342000@gmail.com>
2023-03-31 23:38:09 +0530

    DSA Problems

---
+++ Strings/src/Upper_Case_To_Lower.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Upper_Case_To_Lower {
+
+    public static void main(String[] args) {
+        String str = "WeLCoMe To ShaGggab ";
+        touppercase(str);
+    }
+    static void touppercase(String str){
+        char[] s = str.toCharArray();
+        for(int i =0;i< str.length() -1;i++){
+           char ch = s[i];
+            if (ch >= 'A' && ch <= 'Z') {
+                char ansch = (char) (ch - 'A' + 'a');
+
+            }
+            for(char c:s){
+                System.out.print(c);
+            }
+        }
+
+
+    }
+}
